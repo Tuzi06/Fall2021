21b6787137bcb546691d4dedba150a5a00642954
tuzi06 <damienwang21@gmail.com>
2021-09-14 23:38:36 -0700

    c

--- exer1/np_summary.py
+++ exer1/np_summary.py
@@ -4,5 +4,18 @@ data = np.load('monthdata.npz')
 totals = data['totals']
 counts = data['counts']
 
-sum = np.sum(totals,1)
-print(np.argmin(sum));
+sum_city = np.sum(totals,1)
+print('Row with lowest total precipitation: \n',np.argmin(sum_city))
+
+total_part =np.sum(totals,axis =0)
+totals_ob = np.sum(counts,axis= 0)
+print ('Average precipitation in each month: \n',total_part / totals_ob)
+
+total_part =np.sum(totals,axis =1)
+totals_ob = np.sum(counts,axis= 1)
+print ('Average precipitation in each city:\n',total_part / totals_ob)
+
+total_reshape = np.reshape(totals,(4*len(totals),3))
+total_quad=np.sum(total_reshape,1)
+total_quad.shape=(len(totals),4)
+print('Quarterly precipitation totals:\n',total_quad)
