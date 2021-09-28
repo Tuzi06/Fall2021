74278764467b1e7c4ed1603646d0e8444ff9a262
tuzi06 <damienwang21@gmail.com>
2021-09-27 20:29:58 -0700

    aa

--- e3/smooth_temperature.py
+++ e3/smooth_temperature.py
@@ -10,14 +10,13 @@ def to_timestamp(d):
     return d.timestamp()
 
 cpu_data['timeData'] = pd.to_datetime(cpu_data['timestamp']).apply(to_timestamp)
-print(cpu_data.dtypes)
 
 loess_smoothed = lowess(cpu_data['temperature'],cpu_data['timeData'],frac = 0.01)
 
 kalman_data = cpu_data[['temperature', 'cpu_percent', 'sys_load_1', 'fan_rpm']]
 initial_state = kalman_data.iloc[0]
 observation_covariance = np.diag([0.9, 0.9, 0.9, 0.9]) ** 2 # TODO: shouldn't be zero
-transition_covariance = np.diag([0.02, 0.02, 0.02, 0.02]) ** 2 # TODO: shouldn't be zero
+transition_covariance = np.diag([0.02, 0.02, 0.2, 0.02]) ** 2 # TODO: shouldn't be zero
 transition = [[0.97, 0.5, 0.2, -0.001], [0.1,0.4,2.2,0], [0,0,0.95,0], [0,0,0,1]] # TODO: shouldn't (all) be zero 
 
 
@@ -34,7 +33,7 @@ plt.figure(figsize=(12, 4))
 
 plt.plot(cpu_data['timestamp'], cpu_data['temperature'], 'b.', alpha=0.5)
 plt.legend('temperature data')
-plt.plot(cpu_data['timestamp'], kalman_smoothed[:, 0], 'y-',linewidth=4)
+plt.plot(cpu_data['timestamp'], kalman_smoothed[:, 0], 'y-')
 plt.legend('alman_smooth')
 plt.plot(cpu_data['timestamp'], loess_smoothed[:, 1], 'r-')
 plt.legend('loess_smooth')
