1fbe11cc13875e309e76ee5e659ef085f989e0db
tuzi06 <damienwang21@gmail.com>
2021-10-06 14:22:32 -0700

    sdf

--- e4/average_ratings.py
+++ e4/average_ratings.py
@@ -1,6 +1,14 @@
 import pandas as pd
 import numpy as np
 import sys
+import difflib
 
 
-rating = pd.read_csv(sys.argv[1])
+movie_list= open(sys.argv[1]).readlines()
+rating = pd.read_csv(sys.argv[2])
+
+# print (movie_list)
+
+movie = difflib.get_close_matches('Man on Fire',movie_list)
+
+print(movie)
